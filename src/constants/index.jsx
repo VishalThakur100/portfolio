cd663a1051f4085e53c0e40e98994afa9fe8d0c7
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-05-19 01:50:37 +0530

    foodmood added

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,16 +5,16 @@ import nodejs from "../assets/nodejs.png";
 import reactjs from "../assets/reactjs.png";
 import mongodb from "../assets/mongodb.png";
 import javascript from "../assets/javascript.png";
-import reacr from '../assets/reacr.png'
+import reacr from "../assets/reacr.png";
 import web from "../assets/web.png";
 // import ux from "../assets/ux.png";
 // import typescript from "../assets/typescript.png";
 import git from "../assets/git.png";
 import github from "../assets/github.png";
 import java from "../assets/java.png";
-import pro1 from '../assets/pro1.jpg'
-import pro2 from '../assets/pro2.jpg'
-import pro3 from '../assets/pro3.jpg'
+import pro1 from "../assets/pro1.jpg";
+import pro2 from "../assets/pro2.jpg";
+import pro3 from "../assets/pro3.jpg";
 
 const services = [
   {
@@ -80,29 +80,28 @@ const technologies = [
     name: "github",
     icon: github,
   },
-  
 ];
 
 const projects = [
   {
     name: "foodMood",
     description:
-      "",
+      "foodMood is a cutting-edge food ordering platform designed with the modern epicurean in mind. Crafted using the robust ReactJS framework for dynamic and interactive user interface, styled with the sleek Tailwind CSS which provides a customizable design system that adapts to any device, and managed with the efficient Redux Toolkit that streamlines state management, making your online ordering process smooth and reliable. foodMood isn’t just a website; it’s your passport to a world of culinary delights, all made possible by the synergy of ReactJS, Tailwind CSS, and Redux Toolkit.      ",
     tags: [
       {
-        name: "Typescript",
+        name: "JavaScript",
         color: "blue-text-gradient",
       },
       {
-        name: "Angular",
+        name: "ReactJS",
         color: "green-text-gradient",
       },
       {
-        name: "Bootstrap",
+        name: "TalwindCSS",
         color: "pink-text-gradient",
       },
     ],
-     image: pro1,
+    image: pro1,
     source_code_link: "https://github.com/riva211/realestate.github.io",
     source_deploy_link: "https://rivasrealestate.netlify.app ",
   },
@@ -127,12 +126,11 @@ const projects = [
     image: pro2,
     source_code_link: "https://github.com/riva211?tab=repositories",
     source_deploy_link: "https://rivastodolist.netlify.app",
-
   },
   {
     name: "OxiBook ",
     description:
-    "OxiBook is online book buying and selling website with and user freindly UI. It is a MERN stack web application, ",
+      "OxiBook is online book buying and selling website with and user freindly UI. It is a MERN stack web application, ",
     tags: [
       {
         name: "React js",
@@ -150,7 +148,6 @@ const projects = [
     image: pro3,
     source_code_link: "https://github.com/riva211?tab=repositories",
     source_deploy_link: "https://gregarious-sorbet-33fff8.netlify.app/",
-
   },
 ];
-export { services, technologies,  projects };
+export { services, technologies, projects };
