556da0c9a48b4c4a88d2382cebb4416bd5600f16
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-10-28 01:13:52 +0530

    readem updated

---
+++ apikey.js
@@ -0,0 +1,4 @@
+export default {
+    USER_ID: window.REACT_APP_USER_ID,
+    TEMPLATE_ID: window.REACT_APP_TEMPLATE_ID
+  }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './globals.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./globals.css";
+import dotenv from "dotenv";
 
-ReactDOM.render(
+dotenv.config();
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
